a76c26d24f701d3bb82153df2c3da02bab772b5b
Suhan Saha <suhansaha@gmail.com>
2018-04-28 10:31:21 +0530

    test2

--- index.php
+++ index.php
@@ -13,10 +13,12 @@
 
     class wechatCallbackapiTest
     {
+        echo "Hello World:1"
     	public function valid()
         {
             $echoStr = $_GET["echostr"];
 
+                echo "Hello World:2"
             //valid signature , option
             if($this->checkSignature()){
             	echo $echoStr;
